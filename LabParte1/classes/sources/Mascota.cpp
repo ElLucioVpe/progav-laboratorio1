1d6dd8d9036fed094dbad07ca4c2fc61fa1004d6
Luciano Porta <lucianoporta@outlook.com>
2019-04-04 19:00:18 -0300

    Arreglado el nombre de una variable

--- LabParte1/classes/sources/Mascota.cpp
+++ LabParte1/classes/sources/Mascota.cpp
@@ -1,6 +1,6 @@
 #include "../headers/Mascota.h"
 
-Mascota::Mascota(std::string n, float p, const Genero& gen) : gen(gen) {
+Mascota::Mascota(std::string n, float p, const Genero& gen) : gen(genero) {
 	this->Nombre = n;
 	this->Peso = p;
 }
