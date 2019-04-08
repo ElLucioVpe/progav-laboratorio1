fa85d95eafb3ad498ca940967ef27211d8662d75
Luciano <lucianoporta@outlook.com>
2019-04-08 16:45:09 -0300

    Arreglada clase Perro

--- LabParte1/classes/sources/Perro.cpp
+++ LabParte1/classes/sources/Perro.cpp
@@ -1,7 +1,7 @@
 #include "../headers/Perro.h"
 #include "../../datatypes/headers/RazaPerro.h"
 
-Perro::Perro(RazaPerro raza, boolean VacunaCachorro) : Mascota(std::string NombreMascota, Genero& Genero, float Peso) {
+Perro::Perro(RazaPerro raza, bool VacunaCachorro, std::string NombreMascota, Genero& Genero, float Peso) : Mascota(NombreMascota, Genero, Peso) {
 	this->Raza = Raza;
 	this->VacunaCachorro = VacunaCachorro;
 }
