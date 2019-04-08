19e560689613df31818601b66f578d809ff00cd2
Luciano <lucianoporta@outlook.com>
2019-04-08 16:44:57 -0300

    Arreglada clase Gato

--- LabParte1/classes/headers/Gato.h
+++ LabParte1/classes/headers/Gato.h
@@ -1,14 +1,16 @@
 #ifndef GATO_H
 #define GATO_H
+
 #include "Mascota.h"
 #include "../../datatypes/headers/TipoPelo.h"
+
 class Gato : public Mascota {
   public:
-    Gato(TipoPelo Tipopelo);
+    Gato(TipoPelo _tp, std::string _nombre, Genero _genero, float _peso);
     TipoPelo getTipoPelo();
   private:
-    TipoPelo pelo;
-}
+    TipoPelo Pelo;
+};
 
 
 #endif
