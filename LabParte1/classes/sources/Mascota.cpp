3d8e7d64853fea932fd2b07ee46e99e71f784f66
Luciano Porta <lucianoporta@outlook.com>
2019-04-11 20:41:44 -0300

    obtenerRacionDiaria en Mascotas esta al pedo

--- LabParte1/classes/sources/Mascota.cpp
+++ LabParte1/classes/sources/Mascota.cpp
@@ -5,7 +5,3 @@ Mascota::Mascota(std::string _nombre, Genero& _genero, float _peso) : genero(_ge
 	this->Nombre = _nombre;
 	this->Peso = _peso;
 }
-
-float Mascota::obtenerRacionDiaria() {
-    return 0;
-}
